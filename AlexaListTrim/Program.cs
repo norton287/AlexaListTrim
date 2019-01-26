6274f2d5376d54ced16aeb8eef10fe46dbe4e2c1
John Norton <norton287@gmail.com>
2019-01-26 11:49:09 -0600

    Initial Commit

--- AlexaListTrim/Program.cs
+++ AlexaListTrim/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,37 @@ namespace AlexaListTrim
     {
         static void Main(string[] args)
         {
+            var txtString = new List<string>();
+            var modTxtString = new List<string>();
+
+            using (var sr = new StreamReader(@"c:\temp\text.txt"))
+            {
+                while (sr.Peek() >= 0)
+                    txtString.Add(sr.ReadLine());
+            }
+
+            foreach (var food in txtString)
+            {
+                Console.WriteLine($"Trimming: {food}");
+                if (food.Length <= 0 || (food.IndexOf(".", StringComparison.Ordinal) + 2).ToString() == string.Empty ||
+                    !food.Contains(".")) continue;
+                var newFood = food.Substring(food.IndexOf(".", StringComparison.Ordinal) + 2) + "\n";
+                modTxtString.Add(newFood);
+            }
+
+            using (var sw = new StreamWriter(@"c:\temp\out.txt"))
+            {
+                foreach (var food in modTxtString)
+                {
+                    sw.Write(food);
+                }
+
+                sw.Close();
+            }
+
+            Console.WriteLine("All done!  Press any key to start Notepad.exe");
+            Console.ReadKey();
+            Process.Start("Notepad.exe", @"c:\temp\out.txt");
         }
     }
 }
