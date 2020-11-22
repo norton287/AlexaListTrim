4c242ff1daa2933070f067afde786e93dd6c7798
John Norton <42098364+norton287@users.noreply.github.com>
2020-11-22 07:40:54 -0600

    Refactored code

--- AlexaListTrim/Program.cs
+++ AlexaListTrim/Program.cs
@@ -14,32 +14,28 @@ namespace AlexaListTrim
 
             if (File.Exists(@"c:\temp\text.txt"))
             {
-                using (var sr = new StreamReader(@"c:\temp\text.txt"))
-                {
-                    while (sr.Peek() >= 0)
-                    {
-                        txtString.Add(sr.ReadLine());
-                    }
-                }
+	            using var sr = new StreamReader(@"c:\temp\text.txt");
+	            while (sr.Peek() >= 0)
+	            {
+		            txtString.Add(sr.ReadLine());
+	            }
 
-                foreach (var food in txtString)
-                {
+	            foreach (var food in txtString)
+                 {
                     Console.WriteLine($@"Trimming: {food}");
                     if (food.Length <= 0 || (food.IndexOf(".", StringComparison.Ordinal) + 2).ToString() == string.Empty ||
                         !food.Contains(".")) continue;
                     var newFood = food.Substring(food.IndexOf(".", StringComparison.Ordinal) + 2) + "\r\n";
                     modTxtString.Add(newFood);
-                }
+                 }
 
-                using (var sw = new StreamWriter(@"c:\temp\out.txt"))
-                {
-                    foreach (var food in modTxtString)
-                    {
-                        sw.Write(food);
-                    }
-                }
+	            using var sw = new StreamWriter(@"c:\temp\out.txt");
+	            foreach (var food in modTxtString)
+	            {
+		            sw.Write(food);
+	            }
 
-                Console.WriteLine(@"All done!  Press any key to start Notepad.exe");
+	            Console.WriteLine(@"All done!  Press any key to start Notepad.exe");
                 Console.ReadKey();
                 File.Delete(@"c:\temp\text.txt");
                 Process.Start("Notepad.exe", @"c:\temp\out.txt");
